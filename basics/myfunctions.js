d7362547ad49f581e54324f0d8d3bb046da5cfbe
Ezechiel Iti <papaezechiel@sympatico.ca>
2022-11-18 15:47:59 -0500

    type and function

---
+++ basics/myfunctions.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+function addTwo(num) {
+    // return num + 2;
+    return "hello";
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+// addTwo(5);
+var myValue = addTwo(5);
+getUpper("ezechiel");
+signUpUser("Ezechiel", "ezechiel@gmail.com", false);
+loginUser("Eze", "eze@gmail.com");
---
+++ basics/myfunctions.ts
@@ -0,0 +1,25 @@
+function addTwo(num: number) {
+  // return num + 2;
+  return "hello"
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+function signUpUser(
+  name: string,
+  email: string,
+  isPaid: boolean
+) {}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {
+
+}
+// addTwo(5);
+let myValue = addTwo(5)
+getUpper("ezechiel");
+signUpUser("Ezechiel", "ezechiel@gmail.com", false)
+loginUser("Eze", "eze@gmail.com")
+
+export {}
