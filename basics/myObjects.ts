9ed34bd0a18578b1d8a565ca85f3ea2e6f636f0e
Ezechiel Iti <papaezechiel@sympatico.ca>
2022-11-19 15:41:32 -0500

    type alias

--- basics/myObjects.ts
+++ basics/myObjects.ts
@@ -4,14 +4,25 @@ const User = {
   isActive: true
 }
 
-function createUser({name: string, isPaid: boolean}){}
+// function createUser({name: string, isPaid: boolean}){}
 
-let newUser = {name: "Thomas", isPaid: false, email: "thomas@h.com"}
+// let newUser = {name: "Thomas", isPaid: false, email: "thomas@h.com"}
 
-createUser(newUser)
+// createUser(newUser)
 
 function createCourse():{name: string, price: number}{
   return {name: "ReactJS", price: 99.9}
 }
 
+// type alias
+
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean
+}
+
+function createUser(user: User): User{
+  return {name}
+}
 export {}
