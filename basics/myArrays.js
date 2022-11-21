32e3689ea5724b45c859ff56b7aac66b48748e46
Ezechiel Iti <papaezechiel@sympatico.ca>
2022-11-20 22:27:20 -0500

    Arrays

---
+++ basics/myArrays.js
@@ -0,0 +1,11 @@
+var superHeros = [];
+var heroPower = [];
+var heroDetails = [];
+var allUsers = [];
+var MLModels = [
+    [255, 355, 455],
+    []
+];
+superHeros.push("spider");
+heroPower.push(4);
+allUsers.push({ name: "", isActive: true });
---
+++ basics/myArrays.ts
@@ -0,0 +1,21 @@
+const superHeros: string[] = []
+const heroPower: number[] = []
+
+const heroDetails: Array<number> = []
+
+type User = {
+  name: string
+  isActive: boolean
+}
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+  [255, 355, 455],
+  []
+]
+
+superHeros.push("spider")
+heroPower.push(4)
+
+allUsers.push({name: "", isActive: true})
