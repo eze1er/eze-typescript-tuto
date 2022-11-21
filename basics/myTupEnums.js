e45a66b316fa33601ad47c7cabd12ceb760cfaf2
Ezechiel Iti <papaezechiel@sympatico.ca>
2022-11-20 23:26:56 -0500

    TupEnums

---
+++ basics/myTupEnums.js
@@ -0,0 +1,7 @@
+// const users: (string | number)[] = [1, "eze"]
+var tUser;
+tUser = ['eze', 132, true];
+var rgb = [255, 132, 112];
+var newTUser = [112, 'iti@hotmail.com'];
+newTUser[1] = "eze@gmail.com";
+newTUser.push("true");
---
+++ basics/myTupEnums.ts
@@ -0,0 +1,13 @@
+// const users: (string | number)[] = [1, "eze"]
+let tUser: [string, number, boolean]
+
+tUser = ['eze', 132, true]
+
+let rgb: [number, number, number] = [255, 132, 112]
+
+type TUser = [number, string]
+
+const newTUser: TUser = [112, 'iti@hotmail.com']
+
+newTUser[1] = "eze@gmail.com"
+newTUser.push("true")
