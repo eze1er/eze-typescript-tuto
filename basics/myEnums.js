4f25691a1d2ef81e9959be3a0325f87ae4d696be
Ezechiel Iti <papaezechiel@sympatico.ca>
2022-11-21 00:23:54 -0500

    Enums

---
+++ basics/myEnums.js
@@ -0,0 +1,8 @@
+var SeatChoice;
+(function (SeatChoice) {
+    SeatChoice["AISLE"] = "aisle";
+    SeatChoice[SeatChoice["MIDDLE"] = 4] = "MIDDLE";
+    SeatChoice[SeatChoice["WINDOW"] = 5] = "WINDOW";
+    SeatChoice[SeatChoice["FOURTH"] = 6] = "FOURTH";
+})(SeatChoice || (SeatChoice = {}));
+var hcSeat = SeatChoice.AISLE;
---
+++ basics/myEnums.ts
@@ -0,0 +1,8 @@
+enum SeatChoice {
+  AISLE = "aisle",
+  MIDDLE = 4,
+  WINDOW,
+  FOURTH
+}
+
+const hcSeat = SeatChoice.AISLE
