135b9daca7d38975f3415c3cc1445ae7c8857aac
Ezechiel Iti <papaezechiel@sympatico.ca>
2022-12-27 15:25:13 -0500

    modif

--- purets/src/index.ts
+++ purets/src/index.ts
@@ -1,1 +1,13 @@
-console.log("TypeScript is here!");
+class User {
+  email: string
+  name: string
+  readonly city: string
+  constructor(email: string, name: string){
+    this.email = email;
+    this.name = name;
+    this.city = "";
+  }
+}
+
+const ezechiel = new User( "eze@gmail.com", "Ezechiel")
+// ezechiel.city ="Toronto";
